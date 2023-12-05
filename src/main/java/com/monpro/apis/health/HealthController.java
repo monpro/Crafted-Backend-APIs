e102bbfc62cc004c98abf936644483ca8d8df800
monpro <monproshen@gmail.com>
2023-12-05 15:09:25 +1100

    init api project with health check api

---
+++ src/main/java/com/monpro/apis/health/HealthController.java
@@ -0,0 +1,21 @@
+package com.monpro.apis.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    private final Environment environment;
+
+    @Autowired
+    public HealthController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return String.format("System is running on port %s", environment.getProperty("server.port"));
+    }
+}
